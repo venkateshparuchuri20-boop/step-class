f2b713cfda6f30f047678e021b0630cff5d864cb
Venkatesh <venkateshparuchuri20@gmail.com>
2026-02-23 22:44:22 +0530

    coded uc4

--- step.java
+++ step.java
@@ -1,15 +1,27 @@
-public class step{
+/*
+ * OOPSBannerApp UC4 – Render OOPS as Banner
+ * Using String Array and For-Each Loop
+ */
+
+public class step {
 
     public static void main(String[] args) {
 
-        // OOPS Banner using String.join()
+        // String array to store banner lines
+        String[] lines = new String[7];
 
-        System.out.println(String.join(" ", "  * ", "   * ", " ** ", "    ** "));
-        System.out.println(String.join(" ", "*   *", "*   *", "*   *", " **    "));
-        System.out.println(String.join(" ", "*   *", "*   *", "*   *", "**    "));
-        System.out.println(String.join(" ", "*   *", "*   *", "**  ", " ***  "));
-        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "    * "));
-        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "    * "));
-        System.out.println(String.join(" ", "  * ", "   * ", " *     ", "** "));
+        // OOPS Banner Design
+        lines[0] = String.join(" ", " ***  ", " ***  ", " ***** ", " ***** ");
+        lines[1] = String.join(" ", "**  **", "**  **", "**    * ","**     ");
+        lines[2] = String.join(" ", "**  **", "**  **", "*****  ", " *****  ");
+        lines[3] = String.join(" ", "**  **", "**  **", "**         ", "   **");
+        lines[4] = String.join(" ", "**  **", "**  **", "**         ", "   **");
+        lines[5] = String.join(" ", "**  **", "**  **", "**     ", "**  **");
+        lines[6] = String.join(" ", " ***  ", " ***  ", " ***** ", " ****  ");
+
+        // Print banner using for-each loop
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
