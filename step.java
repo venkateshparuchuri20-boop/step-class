7769bc109eef14753f7d1202924e09f2154e4d83
Venkatesh <venkateshparuchuri20@gmail.com>
2026-02-23 22:54:19 +0530

    coded uc5

--- step.java
+++ step.java
@@ -1,25 +1,22 @@
-/*
- * OOPSBannerApp UC4 – Render OOPS as Banner
- * Using String Array and For-Each Loop
- */
 
-public class step {
+
+public class step{
 
     public static void main(String[] args) {
 
-        // String array to store banner lines
-        String[] lines = new String[7];
+        
+        String[] lines = {
 
-        // OOPS Banner Design
-        lines[0] = String.join(" ", " ***  ", " ***  ", " ***** ", " ***** ");
-        lines[1] = String.join(" ", "**  **", "**  **", "**    * ","**     ");
-        lines[2] = String.join(" ", "**  **", "**  **", "*****  ", " *****  ");
-        lines[3] = String.join(" ", "**  **", "**  **", "**         ", "   **");
-        lines[4] = String.join(" ", "**  **", "**  **", "**         ", "   **");
-        lines[5] = String.join(" ", "**  **", "**  **", "**     ", "**  **");
-        lines[6] = String.join(" ", " ***  ", " ***  ", " ***** ", " ****  ");
+            String.join(" ", " *** ", "  *** ", " ***** ", "  ***** "),
+            String.join(" ", "**  **", "**  **", "**   *  ", "**     "),
+            String.join(" ", "**  **", "**  **", "*****  ", "*****  "),
+            String.join(" ", "**  **", "**  **", "**         ", "  **"),
+            String.join(" ", "**  **", "**  **", "**         ", "  **"),
+            String.join(" ", "**  **", "**  **", "**     ", "**  **"),
+            String.join(" ", " *** ", "  *** ", " ***** ", " **** ")
+        };
 
-        // Print banner using for-each loop
+        
         for (String line : lines) {
             System.out.println(line);
         }
