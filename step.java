517b6c9fb16e188ed1fbe0efb118756a2e051042
Venkatesh <venkateshparuchuri20@gmail.com>
2026-02-23 22:23:53 +0530

    coded uc3

--- step.java
+++ step.java
@@ -1,14 +1,15 @@
 public class step{
-	public static void main(String[] args){
-		System.out.println("    ***       ***    ******       ***** ");
-		System.out.println("  **   **   **   **  **    **   **      ");
-		System.out.println(" **     ** **     ** **     ** **       ");
-	    System.out.println(" **     ** **     ** **    **   **      ");
-		System.out.println(" **     ** **     ** ******      ***    ");
-		System.out.println(" **     ** **     ** **             **  ");
-		System.out.println(" **     ** **     ** **               **");
-		System.out.println("  **   **   **   **  **              ** ");
-		System.out.println("    ***       ***    **          *****  ");	
-	}
-    
+
+    public static void main(String[] args) {
+
+        // OOPS Banner using String.join()
+
+        System.out.println(String.join(" ", "  * ", "   * ", " ** ", "    ** "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*   *", " **    "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*   *", "**    "));
+        System.out.println(String.join(" ", "*   *", "*   *", "**  ", " ***  "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "    * "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "    * "));
+        System.out.println(String.join(" ", "  * ", "   * ", " *     ", "** "));
     }
+}
